223db173d06d1fb5e555fcfe887ae6d3ef626ff0
SRB77 <soumyach144@gmail.com>
2024-08-31 16:26:19 +0530

    introduction to dynamic programming with memoization code

---
+++ DP/Problem1.py
@@ -0,0 +1,32 @@
+# introduction to DYNAMIC PROGRAMMING 
+
+# PROBLEM 1 : PRINT THE Fibonacci number of the given index . usign three techniques of DP . (MEMOIZATION ,TABLUATION, SPACE OTIMIZATION).
+
+
+
+# 1.Memoization (option1)
+def fibo(n,dp=None):
+    if dp is None:
+        dp = [-1]*(n+1)
+    if n ==1:
+        return 1
+    if n == 0:
+        return 0
+    if dp[n] != -1:
+        return dp[n]
+    dp[n]=fibo(n-1,dp)+fibo(n-2,dp)
+    return dp[n]
+print(fibo(8));
+
+# MEMOIZATION (option2)
+def fibo2(n):
+    dp = [-1]*(n+1)
+    def find(ind,dp):
+        if n ==0:return 0
+        if n ==1:return 1
+        if dp[ind] != -1:
+            return dp[ind]
+        dp[ind] =  find(ind-1,dp)+find(ind-2,dp)
+        return  find(ind-1,dp)+find(ind-2,dp)
+    return find(n,dp)
+print(fibo(8))
